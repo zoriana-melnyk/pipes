497c1772f4dd925acc843a4fd9c68f89782350ce
zoriana-melnyk <ziraolena@gmail.com>
2023-11-23 16:34:53 +0200

    feat: modified product card

--- src/app/containers/Products.js
+++ src/app/containers/Products.js
@@ -6,21 +6,29 @@ import pipes_warm from '../img/pipes_warm.png';
 
 function Products() {
   return (
-    <div className="grid my-6 gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+    <div className="grid my-6 gap-6 mb-8 mx-10 md:grid-cols-2 xl:grid-cols-4">
       <ProductCard title="Плівка поліетиленова"
         text="Плівка поліетиленова: рукав, пів рукав, полотно.Первинна, вторинна, кольорова."
-        image={pipes_film} />
+        image={pipes_film}
+        price={'10 ₴/пог.м'}
+      />
       <ProductCard title="Труба для теплої підлоги"
         text="Труба для теплої підлоги SLQ Tece 16x2"
-        image={pipes_floor} />
+        image={pipes_floor}
+        price={'20 ₴/пог.м'}
+      />
       <ProductCard
         title="Поліетиленова труба"
         text="Поліетиленова труба синя первинка ф32 х 2.5-3.0-3,4мм внутрішня стінка"
-        image={pipes_wal} />
+        image={pipes_wal}
+        price={'від 14.25 ₴/пог.м'}
+      />
       <ProductCard
         title="Труба для утеплення"
         text="Труба 16X2 PEX-A для опалення.Для підігрівання підлоги.PEX."
-        image={pipes_warm} />
+        image={pipes_warm}
+        price={'17 ₴/пог.м'}
+      />
     </div>
   );
 }

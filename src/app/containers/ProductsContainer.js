ad84d38423a16f8c44a54c9e9c63edfe703406a5
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2023-12-05 23:06:07 +0200

    feat: pagination for the products list

--- src/app/containers/ProductsContainer.js
+++ src/app/containers/ProductsContainer.js
@@ -1,16 +1,21 @@
 import { useContext, useEffect, useState } from 'react';
 import { ProductCard } from '../components';
-import { Spinner } from 'flowbite-react';
+import { Spinner, Pagination } from 'flowbite-react';
 import { AppContext } from '../service/AppContext';
 import { ADD_PRODUCT } from '../service/contextDispatchTypes';
 
 function Products() {
   const [producs, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const { dispatch, selectedProducts } = useContext(AppContext);
 
   const addToCart = (product) => {
     dispatch({ type: ADD_PRODUCT, product });
   };
+
+  const onPageChange = (page) => setCurrentPage(page);
+  const perPage = 8;
+  const pagesAmount = Math.ceil(producs.length / perPage);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,21 +27,31 @@ function Products() {
   }, []);
 
   return (
-    <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+    <div className="flex flex-col items-center">
       {producs.length ? (
-        producs.map((product) => (
-          <ProductCard
-            key={product._id}
-            product={product}
-            actions={{ addToCart }}
-            isSelected={selectedProducts.some(
-              (selectedProduct) => selectedProduct._id === product._id
-            )}
-          />
-        ))
+        <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+          {producs.map((product) => (
+            <ProductCard
+              key={product._id}
+              product={product}
+              actions={{ addToCart }}
+              isSelected={selectedProducts.some(
+                (selectedProduct) => selectedProduct._id === product._id
+              )}
+            />
+          ))}
+        </div>
       ) : (
         <Spinner size="xl" />
       )}
+
+      {producs.length ? (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={3}
+          onPageChange={onPageChange}
+        />
+      ) : null}
     </div>
   );
 }

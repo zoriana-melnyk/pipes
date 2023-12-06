641bd391596cc60370586f116e207a393ad74e78
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2023-12-06 15:00:18 +0200

    feat: better pagination approach for search and products pages

--- src/app/containers/SearchContainer.js
+++ src/app/containers/SearchContainer.js
@@ -1,27 +1,56 @@
 import { useContext, useEffect, useState } from 'react';
-import { Label, Spinner, TextInput } from 'flowbite-react';
+import { Label, Spinner, TextInput, Pagination } from 'flowbite-react';
 import { ProductCard } from '../components';
 import { AppContext } from '../service/AppContext';
 import { ADD_PRODUCT } from '../service/contextDispatchTypes';
 
 const SearchContainer = () => {
+  const perPage = 4;
   const [query, setQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleSearch = (event) => {
     setQuery(event.target.value);
   };
 
-  const [producs, setProducts] = useState([]);
+  const [producs, setProducts] = useState({
+    list: [],
+    initialList: [],
+    totalPages: 0,
+  });
+
+  const onPageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('/api/product');
       const { data } = await response.json();
-      setProducts(data);
+      setProducts({
+        list: data,
+        initialList: data,
+        totalPages: Math.ceil(data.length / perPage),
+      });
     };
     fetchData();
   }, []);
 
-  const filteredProducts = producs.filter((product) => {
+  useEffect(() => {
+    const start = (currentPage - 1) * perPage;
+    const end = start + perPage;
+    setProducts({
+      ...producs,
+      list: producs.initialList.slice(start, end),
+    });
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
+
+  const filteredProducts = producs.list.filter((product) => {
     return product.name.toLowerCase().includes(query.toLowerCase());
   });
   const { dispatch, selectedProducts } = useContext(AppContext);
@@ -35,14 +64,14 @@ const SearchContainer = () => {
       <div className="my-4">
         <Label>Search Products</Label>
         <TextInput
-          type="text"
+          type="search"
           value={query}
           onChange={handleSearch}
           placeholder="Search for products..."
         />
       </div>
       <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-        {producs.length ? (
+        {producs.list.length ? (
           filteredProducts.map((product, index) => (
             <ProductCard
               key={product._id}
@@ -57,6 +86,14 @@ const SearchContainer = () => {
           <Spinner size="xl" />
         )}
       </div>
+
+      {producs.totalPages > 1 && !query ? (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={producs.totalPages}
+          onPageChange={onPageChange}
+        />
+      ) : null}
     </>
   );
 };

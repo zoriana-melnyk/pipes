65cb89de32bc7fefca5dca8e9d2a5abc71dd98aa
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2023-12-16 22:09:39 +0200

    feat: meke react context global available

--- src/app/HOC/skeleton.js
+++ src/app/HOC/skeleton.js
@@ -1,11 +1,15 @@
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { Suspense, useContext, useEffect, useState } from 'react';
 import { AppFooter, Header } from '../components';
 import { Flowbite, Spinner } from 'flowbite-react';
-import { AppContextProvider } from '../service/AppContext';
 import { Toaster } from 'sonner';
+import { AppContext } from '../service/AppContext';
 
 const Skeleton = ({ children }) => {
   let initialThemeMode = false;
+  const appState = useContext(AppContext);
+  const { user, dispatch } = appState;
+  const storedUser = user || {};
+
   if (typeof window !== 'undefined') {
     // Client-side-only code
     initialThemeMode =
@@ -15,6 +19,7 @@ const Skeleton = ({ children }) => {
 
   const [isDarkMode, setIsDarkMode] = useState(initialThemeMode);
 
+  // theme change listener
   useEffect(() => {
     // listener dor theme change
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
@@ -35,18 +40,32 @@ const Skeleton = ({ children }) => {
     };
   }, [initialThemeMode]);
 
+  // get auth data if user token is stored
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch('/api/user/me', {
+        method: 'POST',
+        body: JSON.stringify({
+          token: storedUser.token || localStorage.getItem('token'),
+        }),
+      });
+      const { data } = await response.json();
+      dispatch({ type: 'SET_USER', payload: data });
+    };
+    fetchUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <Flowbite theme={{ dark: isDarkMode}}>
-      <AppContextProvider>
-        <Header />
-        <Toaster richColors />
-        <Suspense fallback={<Spinner size="xl" />}>
-          <main className="main main__container flex flex-col mx-5 my-3">
-            {children}
-          </main>
-        </Suspense>
-        <AppFooter />
-      </AppContextProvider>
+    <Flowbite theme={{ dark: isDarkMode }}>
+      <Header />
+      <Toaster richColors />
+      <Suspense fallback={<Spinner size="xl" />}>
+        <main className="main main__container flex flex-col mx-5 my-3">
+          {children}
+        </main>
+      </Suspense>
+      <AppFooter />
     </Flowbite>
   );
 };

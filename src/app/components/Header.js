54ecf953b03cb22773099e333b11f47741207018
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2023-12-17 17:47:40 +0200

    feat: loading spinner for logged in user

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,15 +1,42 @@
+'use client';
+
 import Link from 'next/link';
-import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import { Avatar, Dropdown, Navbar, Spinner } from 'flowbite-react';
 import { DarkThemeToggle } from 'flowbite-react';
 import logo from '../img/icon.png';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { AppContext } from '../service/AppContext';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import UserIcon from '../img/icon_user.svg';
 import { REMOVE_USER } from '../service/contextDispatchTypes';
 
+const vocabulary = {
+  en: {
+    home: 'Home',
+    search: 'Search',
+    cart: 'Cart',
+    about: 'About',
+    login: 'Login',
+    logout: 'Logout',
+    settings: 'Settings',
+    notifications: 'Notifications',
+  },
+  ua: {
+    home: 'Головна',
+    search: 'Пошук',
+    cart: 'Кошик',
+    about: 'Про нас',
+    login: 'Вхід',
+    logout: 'Вийти',
+    settings: 'Налаштування',
+    notifications: 'Сповіщення',
+  },
+};
+
 const Header = () => {
+  const lang = 'ua';
+  const t = (key) => vocabulary[lang][key];
   const pathname = usePathname();
   const appState = useContext(AppContext);
   const { selectedProducts, user, dispatch } = appState;
@@ -25,7 +52,7 @@ const Header = () => {
     if (amount) {
       return (
         <>
-          <span className="sr-only">Notifications</span>
+          <span className="sr-only">{t('notifications')}</span>
           <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
             {amount}
           </div>
@@ -37,6 +64,7 @@ const Header = () => {
   const UserSection = ({ children }) => {
     // TODO: login check
     const isLoggedIn = storedUser.email;
+    const token = storedUser.token || localStorage.getItem('token');
 
     if (isLoggedIn) {
       return (
@@ -61,19 +89,21 @@ const Header = () => {
                 {storedUser.email}
               </span>
             </Dropdown.Header>
-            <Navbar.Link
-              as={Link}
-              href="/profile"
-              active={pathname === '/profile'}
-            >
-              <Dropdown.Item>Налаштування</Dropdown.Item>
-            </Navbar.Link>
-            <Navbar.Link as={Link} href="/cart" active={pathname === '/cart'}>
-              <Dropdown.Item className="relative">
-                Кошик
+            <Dropdown.Item>
+              <Navbar.Link
+                as={Link}
+                href="/profile"
+                active={pathname === '/profile'}
+              >
+                {t('settings')}
+              </Navbar.Link>
+            </Dropdown.Item>
+            <Dropdown.Item className="relative">
+              <Navbar.Link as={Link} href="/cart" active={pathname === '/cart'}>
+                {t('cart')}
                 <NotificationBudge />
-              </Dropdown.Item>
-            </Navbar.Link>
+              </Navbar.Link>
+            </Dropdown.Item>
             <Dropdown.Divider />
             <Dropdown.Item onClick={onLogout}>Вийти</Dropdown.Item>
           </Dropdown>
@@ -85,21 +115,27 @@ const Header = () => {
 
     return (
       <div className="flex list-none items-center">
-        <div className="relative mx-3 w-8">
-          <Image
-            src={UserIcon}
-            style={{ aspectRatio: '1 / 1', width: '25px', height: 'auto' }}
-            alt="User-Icon"
-          />
-          <NotificationBudge />
-        </div>
-        <Navbar.Link
-          as={Link}
-          href={isLoggedIn ? '/profile' : '/login'}
-          className="flex items-center list-none border-none text-sm"
-        >
-          Вхід
-        </Navbar.Link>
+        {!isLoggedIn && token ? (
+          <Spinner />
+        ) : (
+          <>
+            <div className="relative mx-3 w-8">
+              <Image
+                src={UserIcon}
+                style={{ aspectRatio: '1 / 1', width: '25px', height: 'auto' }}
+                alt="User-Icon"
+              />
+              <NotificationBudge />
+            </div>
+            <Navbar.Link
+              as={Link}
+              href={isLoggedIn ? '/profile' : '/login'}
+              className="flex items-center list-none border-none text-sm"
+            >
+              {t(isLoggedIn ? 'settings' : 'login')}
+            </Navbar.Link>
+          </>
+        )}
 
         {children}
       </div>
@@ -120,16 +156,16 @@ const Header = () => {
       </Navbar.Brand>
       <Navbar.Collapse>
         <Navbar.Link as={Link} href="/" active={pathname === '/'}>
-          Головна
+          {t('home')}
         </Navbar.Link>
         <Navbar.Link as={Link} href="/search" active={pathname === '/search'}>
-          Пошук
+          {t('search')}
         </Navbar.Link>
         <Navbar.Link as={Link} href="/cart" active={pathname === '/cart'}>
-          Кошик
+          {t('cart')}
         </Navbar.Link>
         <Navbar.Link as={Link} href="/about" active={pathname === '/about'}>
-          Про нас
+          {t('about')}
         </Navbar.Link>
       </Navbar.Collapse>
 

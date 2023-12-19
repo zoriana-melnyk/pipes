1b13860d82c1f73e5d0c2657f30921d993c3e703
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2023-12-19 11:28:02 +0200

    chore: clean up

--- src/app/components/UserOrders.js
+++ src/app/components/UserOrders.js
@@ -8,20 +8,42 @@ export const UserOrders = ({ orders }) => {
   const StatusBudge = (status) => {
     switch (status) {
       case 'pending':
-        return <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">Обробляється</span>
+        return (
+          <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
+            Обробляється
+          </span>
+        );
       case 'completed':
-        return <span class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Завершено</span>
+        return (
+          <span class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+            Завершено
+          </span>
+        );
       case 'cancelled':
-        return <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">Відхилено</span>
+        return (
+          <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
+            Відхилено
+          </span>
+        );
       case 'processing':
-        return <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">Доставляється</span>
+        return (
+          <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+            Доставляється
+          </span>
+        );
       default:
-        return <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">Невідомо</span>
+        return (
+          <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+            Невідомо
+          </span>
+        );
     }
-  }
+  };
   return (
     <div className="overflow-x-auto my-4">
-      <Label className="flex w-full text-center justify-self-center">Ваші замовлення</Label>
+      <Label className="flex w-full text-center justify-self-center">
+        Ваші замовлення
+      </Label>
       <Table>
         <Table.Head>
           <Table.HeadCell>Товари</Table.HeadCell>
